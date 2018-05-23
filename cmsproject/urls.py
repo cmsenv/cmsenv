61f0c759c0782db62a02573835524098aeabead0
cmsenv <39554333+cmsenv@users.noreply.github.com>
2018-05-23 16:13:54 +0530

    Add files via upload

---
+++ cmsproject/urls.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from cms.sitemaps import CMSSitemap
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns
+from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve
+from django.conf.urls.static import static
+from quanti.views import *
+
+urlpatterns = [
+	url(r'^admin', include(admin.site.urls)),
+	#url(r'^', home),
+	
+	url(r'^', include('cms.urls'))
+]
+
+# This is only needed when using runserver.
+if settings.DEBUG is True:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
